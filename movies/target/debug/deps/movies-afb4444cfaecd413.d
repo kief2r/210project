7d22fc98a6421dd9c4efdbe2a4de142541f7fed8
kiefer <122419128+kief2r@users.noreply.github.com>
2025-05-03 17:30:26 -0400

    Created rust project

---
+++ movies/target/debug/deps/movies-afb4444cfaecd413.d
@@ -0,0 +1,5 @@
+/Users/kiefersherlock/Documents/GitHub/210project1/movies/target/debug/deps/libmovies-afb4444cfaecd413.rmeta: src/main.rs
+
+/Users/kiefersherlock/Documents/GitHub/210project1/movies/target/debug/deps/movies-afb4444cfaecd413.d: src/main.rs
+
+src/main.rs:
---
+++ movies/target/debug/deps/movies-ef70be7ad56dd0d3.d
@@ -0,0 +1,5 @@
+/Users/kiefersherlock/Documents/GitHub/210project1/movies/target/debug/deps/libmovies-ef70be7ad56dd0d3.rmeta: src/main.rs
+
+/Users/kiefersherlock/Documents/GitHub/210project1/movies/target/debug/deps/movies-ef70be7ad56dd0d3.d: src/main.rs
+
+src/main.rs:
